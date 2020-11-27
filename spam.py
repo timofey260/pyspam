543ab2965110ace0c9b22c364633d93cb760740f
timofey260 <69044246+timofey260@users.noreply.github.com>
2020-11-27 20:13:39 +0300

    spam.py

--- spam.py
+++ spam.py
@@ -1,11 +1,24 @@
 import pyautogui
 import time
-b = input('text: ')
-n = int(input('num of messages: '))
-time.sleep(5)
-f = range(0, n)
+from banner import b
+from colorama import Fore, init
+init(Fore)
 
+print(Fore.RED + b)
+print(Fore.GREEN + 'text: ' + Fore.RED)
+b = input()
+print(Fore.GREEN + 'num of messages: ' + Fore.RED)
+n = int(input())
+time.sleep(3)
+f = range(0, n)
+print(Fore.YELLOW + "wait.")
+time.sleep(0.5)
+print(Fore.YELLOW + "wait..")
+time.sleep(0.5)
+print(Fore.YELLOW + "wait...")
 for i in f:
-    print((i + 1), ' message send!')
+    print(Fore.GREEN + '%1d message send!' %(i + 1))
     pyautogui.write(b)
     pyautogui.press('enter')
+
+pyautogui.alert('succefuly send %2d messages!' % n)
